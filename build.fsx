9243f851704db3009f06d35f48eb655335303dad
Alexey Zimarev <alex@leticon.com>
2016-07-10 14:14:50 +0200

    Changed the build script

--- build.fsx
+++ build.fsx
@@ -1,39 +1,96 @@
 // include Fake libs
+#I "packages/FAKE/tools/"
 #r "packages/FAKE/tools/FakeLib.dll"
+#r "packages/FAKE/tools/Fake.Deploy.Lib.dll"
 
 open Fake
 open Fake.Paket
 open Fake.FileUtils
+open Fake.Testing.XUnit2
+open Fake.PaketTemplate
+open Fake.AssemblyInfoFile
+open Fake.ProcessHelper
+open Fake.FileHelper
+open Fake.Json
 open Fake.Testing.NUnit3
+
+let pathInfo = directoryInfo "."
+let product = environVarOrDefault "BAMBOO_productName" pathInfo.Name
+let company = "Alexey Zimarev"
+let copyright = "Copyright © " + System.DateTime.UtcNow.Year.ToString() + " " + company
 
 // Directories
 let rootDir = currentDirectory
 let buildDir  = currentDirectory + "/bin/"
 let testsDir  = currentDirectory + "/tests/"
-let nugetDir = currentDirectory + "/nuget/"
+let nugetDir = currentDirectory + "/nuget"
+let testOutputDir = currentDirectory + "/"
 
+let nugetFeed = environVar "BAMBOO_nugetPublishUrl"
 let nugetApiKey = environVar "BAMBOO_nugetApiKey"
-let nugetUrl = environVar "BAMBOO_nugetPublishUrl"
-let nugetVersion = getBuildParamOrDefault "nugetVersion" null
+
+let gitversion = "gitversion"
 
 // Filesets
-let appReferences  =
+let appReferences =
     !! "src/**/*.csproj"
-      -- "src/**/*.Tests.csproj"
+      ++ "src/**/*.fsproj"
+      -- "src/**/*Tests.csproj"
 
-let testReferences  =
-    !! "src/**/*.Tests.csproj"
+let testReferences =
+    !! "src/**/*Tests.csproj"
+
+type Version = {
+    Major: int
+    Minor: string
+    Patch: string
+    PreReleaseTag: string
+    PreReleaseTagWithDash: string
+    BuildMetaData: string
+    BuildMetaDataPadded: string
+    FullBuildMetaData: string
+    MajorMinorPatch: string
+    SemVer: string
+    LegacySemVer: string
+    LegacySemVerPadded: string
+    AssemblySemVer: string
+    FullSemVer: string
+    InformationalVersion: string
+    BranchName: string
+    Sha: string
+    NuGetVersionV2: string
+    NuGetVersion: string
+    CommitDate: string
+}
+let mutable gitVer = Unchecked.defaultof<Version>
 
 // Targets
 Target "Clean" (fun _ ->
-    CleanDirs [buildDir; nugetDir]
+    CleanDirs [buildDir; testsDir; nugetDir ]
     MSBuildRelease buildDir "Clean" appReferences
         |> Log "Clean-Output: "
 )
 
-Target "BuildTests" (fun _ ->
-    MSBuildRelease testsDir "Build" testReferences
-        |> Log "BuildTests-Output: "
+Target "SetVersion" (fun _ ->
+
+    let result = ExecProcessAndReturnMessages (fun info ->
+        info.FileName <- gitversion
+        info.WorkingDirectory <- "."
+        info.Arguments <- "/output json") (System.TimeSpan.FromMinutes 5.0)
+
+    if result.ExitCode <> 0 then failwithf "'GitVersion.exe' returned with a non-zero exit code"
+
+    let jsonResult = System.String.Concat(result.Messages)
+
+    jsonResult |> deserialize<Version> |> fun ver -> gitVer <- ver
+
+    CreateCSharpAssemblyInfo "./src/SolutionAssemblyInfo.cs"
+        [Attribute.Product product
+         Attribute.Company company
+         Attribute.Copyright copyright
+         Attribute.Version gitVer.AssemblySemVer
+         Attribute.FileVersion (gitVer.MajorMinorPatch + ".0")
+         Attribute.InformationalVersion gitVer.InformationalVersion]
 )
 
 Target "BuildApp" (fun _ ->
@@ -41,20 +98,27 @@ Target "BuildApp" (fun _ ->
         |> Log "BuildApp-Output: "
 )
 
-Target "Push" (fun _ ->
-    Push (fun p ->
-        {p with
-            ApiKey = nugetApiKey
-            PublishUrl = nugetUrl
-            WorkingDir = nugetDir
-        })
+Target "BuildTests" (fun _ ->
+    MSBuildRelease testsDir "Build" testReferences
+        |> Log "BuildTests-Output: "
 )
 
 Target "Pack" (fun _ ->
     Pack (fun p ->
-        let p' = {p with OutputPath = nugetDir; WorkingDir = rootDir + "/src" }
-        if nugetVersion = null then p' else {p' with Version = nugetVersion }
-        )
+        {p with
+            OutputPath = nugetDir
+            WorkingDir = rootDir + "/src"
+            Version = gitVer.NuGetVersionV2
+         })
+)
+
+Target "Push" (fun _ ->
+    Push (fun p ->
+        {p with
+            ApiKey = nugetApiKey
+            PublishUrl = nugetFeed
+            WorkingDir = nugetDir
+        })
 )
 
 Target "Test" (fun _ ->
@@ -67,6 +131,7 @@ Target "Test" (fun _ ->
 
 // Build order
 "Clean"
+  ==> "SetVersion"
   ==> "BuildApp"
   ==> "BuildTests"
   ==> "Test"
@@ -74,4 +139,4 @@ Target "Test" (fun _ ->
   ==> "Push"
 
 // start build
-RunTargetOrDefault "Test"
+RunTargetOrDefault "Pack"
--- src/MassTransit.RavenDbIntegration/Properties/AssemblyInfo.cs
+++ src/MassTransit.RavenDbIntegration/Properties/AssemblyInfo.cs
@@ -8,11 +8,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("MassTransit.RavenDbIntegration")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MassTransit.RavenDbIntegration")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -21,16 +16,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("d82cbf97-2d75-4888-a8b1-b02361c92c87")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
