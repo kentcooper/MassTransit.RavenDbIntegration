cb77d3d8155e2379d2d0a995f2b3df3a309dde78
Alexey Zimarev <alzi@abax.no>
2016-07-15 15:13:44 +0200

    Fixing version constraints

--- src/SolutionAssemblyInfo.cs
+++ src/SolutionAssemblyInfo.cs
@@ -6,10 +6,10 @@ using System.Reflection;
 [assembly: AssemblyCopyrightAttribute("Copyright � 2016 Alexey Zimarev")]
 [assembly: AssemblyVersionAttribute("3.3.0.0")]
 [assembly: AssemblyFileVersionAttribute("3.3.0.0")]
-[assembly: AssemblyInformationalVersionAttribute("3.3.0-unstable.8+Branch.develop.Sha.1a3cbca0b385a235171389506aacf1631923d6d9")]
+[assembly: AssemblyInformationalVersionAttribute("3.3.0-unstable.13+Branch.develop.Sha.f60e918c9adbcf0df6e13112902725b8be30f8db")]
 namespace System {
     internal static class AssemblyVersionInformation {
         internal const string Version = "3.3.0.0";
-        internal const string InformationalVersion = "3.3.0-unstable.8+Branch.develop.Sha.1a3cbca0b385a235171389506aacf1631923d6d9";
+        internal const string InformationalVersion = "3.3.0-unstable.13+Branch.develop.Sha.f60e918c9adbcf0df6e13112902725b8be30f8db";
     }
 }
